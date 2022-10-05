97e793f1890ddd68139eef705775291ba515918e
Akiya-Kudo <k.akiya07@gmail.com>
2022-10-05 09:39:36 +0900

    adjust the top animation

--- components/TopBack.tsx
+++ components/TopBack.tsx
@@ -6,6 +6,29 @@ import  styles  from '../styles/components/Top.module.css';
 const TopBack = () => {
     return (
         <Box className={ logo.boxContainer }>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box1back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box2back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box3back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box4back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box5back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box6back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box7back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box8back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box9back  }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box10back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box11back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box12back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box13back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box14back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box15back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box16back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box17back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box18back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box19back }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box20back }></Image>
+
+
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box_1 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box1 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box2 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box3 }></Image>
@@ -16,6 +39,7 @@ const TopBack = () => {
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box8 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box9 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box10 }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box10side }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box11 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box12 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box13 }></Image>
@@ -24,6 +48,7 @@ const TopBack = () => {
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box16 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box17 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box18 }></Image>
+            <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box19side }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box19 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box20 }></Image>
             <Image h={100} pos="absolute" src='box.svg' alt='' className={ logo.box21 }></Image>
