f66d200dd11d1bdf4bd12add981e964068b8d76c
USER <user@USERnoMacBook-Air.local>
2023-01-11 17:12:07 +0900

    I want to change the editor library, so cut the new branch and delete the files

--- components/Forms/BlogForm.tsx
+++ components/Forms/BlogForm.tsx
@@ -1,12 +1,45 @@
-import { Flex, Heading } from '@chakra-ui/react';
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import { Alert, Box, Flex, Heading, Input } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import React, { Dispatch, SetStateAction, useRef, useState } from 'react'
 import ReactQuill from 'react-quill';
 import { PostProcessType, PostType } from '../../types/post';
 import { PageBackButton } from './postForms';
+import styles from '../../styles/components/my-quill.module.css'
 
 const BlogForm = ({setProgressValue, setPostType, progressValue, setPostProcess}: {setProgressValue: Dispatch<SetStateAction<number>>, setPostType: Dispatch<SetStateAction<PostType>>, progressValue: number, setPostProcess: Dispatch<SetStateAction<PostProcessType>>}) => {
     
     const [value, setValue] = useState()
+
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, false] }],
+            ['bold', 'italic', 'underline','strike', 'blockquote', 'code-block'],
+            [{ 'color': []}, { 'background': []}],
+            [{'list': 'ordered'}, {'list': 'bullet'}],
+            [{'indent': '-1'}, {'indent': '+1'}, {'align': []}],
+            ['link', 'image', "video"],
+            ['clean']
+        ],
+        history: {
+            delay: 2000,maxStack: 500,userOnly: true
+        }
+    }
+    
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 
+        'strike', 'blockquote', 'code-block',
+        'color', 'background',
+        'list', 'bullet', 'indent', 'align',
+        'link', 'image', "video",
+        'history',
+    ]
+
+    let reactQuillRef = useRef<ReactQuill | null>(null)
+
+    const EditorChanged = (e: any) => {
+        setValue(e)
+    }
 
     return (
         <>
@@ -27,11 +60,13 @@ const BlogForm = ({setProgressValue, setPostType, progressValue, setPostProcess}
                 {/* <TagSelectModal/> */}
                 {/* <PublishSwitch/> */}
                 {/* <SaveButton/> */}
-                <ReactQuill theme="snow" value={value} onChange={() =>  setValue }/>
+                <div className={styles.quillContainer}>
+                    <ReactQuill ref={ reactQuillRef } theme={"snow"} value={value} modules={modules} formats={formats} placeholder={"Write Your Tips..."} onChange={ EditorChanged } />
+                </div>
 
             </Flex>
         </>
     )
-}
+} 
 
 export default BlogForm
