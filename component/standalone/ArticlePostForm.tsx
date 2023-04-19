0dcf988006e5b0f4358cfda5a134eef9e487d460
USER <user@USERnoMacBook-Air.local>
2023-04-19 17:44:52 +0900

    editor display successed

--- component/standalone/ArticlePostForm.tsx
+++ component/standalone/ArticlePostForm.tsx
@@ -1,28 +1,84 @@
-import { Box } from "@chakra-ui/react";
+// import dynamic from "next/dynamic";
+import { Box, Flex } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { FlatBord } from "../atom/bords";
 import { NeumFloatFormInput } from "../atom/inputs"
+// import { useEffect, useState } from "react";
+
+import dynamic from "next/dynamic";
+
+const ArticleEditor = dynamic(
+    () => import("../atom/ArticleEditor"),
+    { ssr: false }
+);
+
 
 export const ArticlePostForm = () => {
     const  { register, formState: { errors }, formState, trigger, watch } = useForm({mode: "all"});
     return (
         <>
-            <Box display={"flex"} flexDirection={"column"} w="90%" h="100%">
-                <NeumFloatFormInput
-                id="input_article_title"
-                labelName={"タイトル"} 
-                validation={undefined}
-                errors={errors} register={register} 
-                isRequired
-                borderRadius="15px"
-                my={3}
-                fontWeight={"bold"}
-                />
-                <FlatBord
-                h="60vh"
-                my={3}
-                >editor</FlatBord>
-            </Box>
-        </>
+        <Flex flexDirection={"column"} w="90%" h="100%">
+            <NeumFloatFormInput
+            id="input_article_title"
+            labelName={"タイトル"} 
+            validation={undefined}
+            errors={errors} register={register} 
+            isRequired
+            borderRadius="15px"
+            my={3}
+            fontWeight={"bold"}
+            />
+            <FlatBord
+            h="65vh"
+            my={3}
+            >
+            <ArticleEditor
+            defaultValue={undefined}
+            onChange={(api:any, event:any) =>{}}
+            onReady={() => console.log("ready")}
+            onSave={() => console.log("saved")}
+            />
+            </FlatBord>
+        </Flex>
+    </>
     )
 }
+
+
+
+
+// const ArticleEditor = dynamic(() => import("../atom/ArticleEditor"), { ssr: false, loading: () => <p>Loading ...</p>, });
+
+// export const ArticlePostForm = () => {
+    // const  { register, formState: { errors }, formState, trigger, watch } = useForm({mode: "all"});
+
+//     // let [editorInstance, setEditorInstance] = useState({})
+//     // const handleInstance = (instance: any) => {
+//     //     setEditorInstance(instance)
+//     // }
+
+//     return (
+        // <>
+        //     <Flex flexDirection={"column"} w="90%" h="100%">
+        //         <NeumFloatFormInput
+        //         id="input_article_title"
+        //         labelName={"タイトル"} 
+        //         validation={undefined}
+        //         errors={errors} register={register} 
+        //         isRequired
+        //         borderRadius="15px"
+        //         my={3}
+        //         fontWeight={"bold"}
+        //         />
+        //         <FlatBord
+        //         h="65vh"
+        //         my={3}
+        //         >
+        //             {/* {ArticleEditor && <ArticleEditor handleInstance={handleInstance}/>} */}
+        //             {ArticleEditor && <ArticleEditor/>}
+        //         </FlatBord>
+        //     </Flex>
+        // </>
+//     )
+// }
+
--- pages/user/post_create.tsx
+++ pages/user/post_create.tsx
@@ -29,6 +29,7 @@ const PostCreate: NextPage = () => {
   // useEffect(() => { if (userState !== 'isUser')  router.replace('/') }, [userState])
 
   const handlePublish = (value: boolean) => {
+    console.log(value)
     return value
   }
 
@@ -36,7 +37,7 @@ const PostCreate: NextPage = () => {
     <>
       <PostHeader title={"文章で記録"}>
         <GlassSwitchButton
-        getState={handlePublish} defStateValue={true}
+        getState={handlePublish} defStateValue={false}
         variant={"outline"} fontSize={".9rem"} 
         SBgGradient={"linear(to-tl, tipsy_color_2, tipsy_color_3)"}
         Scolor={"bg_switch"} Acolor={"tipsy_color_active_3"} Hcolor={"tipsy_color_3"}
@@ -63,7 +64,6 @@ const PostCreate: NextPage = () => {
       direction="column" 
       justify="center" 
       align="center"
-      pt={"100px"}
       >
         <ArticlePostForm/>
         {/* <PostPage/> */}
@@ -73,81 +73,81 @@ const PostCreate: NextPage = () => {
 }
 export default PostCreate
 
-type TypeSetButtonProptype =  {setType: any, name: string, height: number, children?: any, bg: string, shadow: string, border: string, minW?: number, Mx?: number}
-const BlogFromContainer = dynamic(() => import("../../components/Forms/BlogForm"), { ssr: false });
+// type TypeSetButtonProptype =  {setType: any, name: string, height: number, children?: any, bg: string, shadow: string, border: string, minW?: number, Mx?: number}
+// const BlogFromContainer = dynamic(() => import("../../components/Forms/BlogForm"), { ssr: false });
 
-const PostPage = () => {
+// const PostPage = () => {
   
-  const [postProcess, setPostProcess] = useState<PostProcessType>("postTopForm")
-  const [progressValue, setProgressValue] = useState(0)
-  const [postType, setPostType] = useState<PostType>(null)
-  const [postTopInfo, setPostTopInfo] = useState<PostTopInfoType>()
+//   const [postProcess, setPostProcess] = useState<PostProcessType>("postTopForm")
+//   const [progressValue, setProgressValue] = useState(0)
+//   const [postType, setPostType] = useState<PostType>(null)
+//   const [postTopInfo, setPostTopInfo] = useState<PostTopInfoType>()
 
-  return (
-    <>
-      <Progress mt={5} colorScheme="red" w={300} hasStripe value={progressValue} />
-      {postProcess == "postTopForm" && <PostTopForm progressValue={progressValue} setProgressValue={ setProgressValue } setPostProcess={setPostProcess} setPostTopInfo={ setPostTopInfo } postTopInfo={postTopInfo}/> }
-      {postProcess == "postTypeSelect" && <SetTypeButtonContainer setProgressValue={ setProgressValue } setPostType={setPostType} setPostProcess={setPostProcess}/> }
-      {postProcess == "contentForm" && postType == "blog" && <BlogFromContainer setProgressValue={ setProgressValue } setPostType={setPostType} progressValue={progressValue} setPostProcess={setPostProcess}/>}
-    </>
-  )
-}
+//   return (
+//     <>
+//       <Progress mt={5} colorScheme="red" w={300} hasStripe value={progressValue} />
+//       {postProcess == "postTopForm" && <PostTopForm progressValue={progressValue} setProgressValue={ setProgressValue } setPostProcess={setPostProcess} setPostTopInfo={ setPostTopInfo } postTopInfo={postTopInfo}/> }
+//       {postProcess == "postTypeSelect" && <SetTypeButtonContainer setProgressValue={ setProgressValue } setPostType={setPostType} setPostProcess={setPostProcess}/> }
+//       {postProcess == "contentForm" && postType == "blog" && <BlogFromContainer setProgressValue={ setProgressValue } setPostType={setPostType} progressValue={progressValue} setPostProcess={setPostProcess}/>}
+//     </>
+//   )
+// }
 
-const SetTypeButtonContainer = ({setPostType, setPostProcess, setProgressValue}: any) => {
+// const SetTypeButtonContainer = ({setPostType, setPostProcess, setProgressValue}: any) => {
 
-  const setTypeToBlog = () => {
-    setPostType("blog")
-    setPostProcess("contentForm")
-    setProgressValue(50)
-  }
-  const setTypeToSlide = () => {
-    setPostType("slide")
-    setPostProcess("contentForm")
-    setProgressValue(60)
-  }
-  const setTypeToLinkOnly = () => {
-    setPostType("linkOnly")
-    setPostProcess("contentForm")
-    setProgressValue(90)
-  }
+//   const setTypeToBlog = () => {
+//     setPostType("blog")
+//     setPostProcess("contentForm")
+//     setProgressValue(50)
+//   }
+//   const setTypeToSlide = () => {
+//     setPostType("slide")
+//     setPostProcess("contentForm")
+//     setProgressValue(60)
+//   }
+//   const setTypeToLinkOnly = () => {
+//     setPostType("linkOnly")
+//     setPostProcess("contentForm")
+//     setProgressValue(90)
+//   }
 
-  return (
-    <>
-      <Heading mt={7} me={20}>
-        <PageBackButton beforePageForm={"postTopForm"} beforeProcessValue={0} setProgressValue={ setProgressValue } setPostProcess={setPostProcess} />
-        Post Tips
-      </Heading>     
-      <Flex mt={3} h="90vh" w="100vw" justify="center" align="center" flexWrap="wrap"> 
-        <TypeSetButton setType={setTypeToBlog} name={"blog"} height={250} bg={"gray.50"} shadow={"inner"} border={"0px"}><FaPen size={45}/></TypeSetButton>
-        <TypeSetButton setType={setTypeToSlide} name={"Slides & Movies"} height={250} bg={"white"} shadow={"xl"} border={"1px solid #eaeaea"}><AiOutlinePicture size={70} /></TypeSetButton>
-        <TypeSetButton setType={setTypeToLinkOnly} name={"Link only"} height={75} bg={"white"} shadow={"xl"} border={"1px solid #eaeaea"} minW={250} Mx={10000}/>
-        {/* 余白調整の空タグ */}
-        <Box w="100%"></Box>
-        <Box w="100%"></Box>
-        <Box w="100%"></Box>
-      </Flex>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <Heading mt={7} me={20}>
+//         <PageBackButton beforePageForm={"postTopForm"} beforeProcessValue={0} setProgressValue={ setProgressValue } setPostProcess={setPostProcess} />
+//         Post Tips
+//       </Heading>     
+//       <Flex mt={3} h="90vh" w="100vw" justify="center" align="center" flexWrap="wrap"> 
+//         <TypeSetButton setType={setTypeToBlog} name={"blog"} height={250} bg={"gray.50"} shadow={"inner"} border={"0px"}><FaPen size={45}/></TypeSetButton>
+//         <TypeSetButton setType={setTypeToSlide} name={"Slides & Movies"} height={250} bg={"white"} shadow={"xl"} border={"1px solid #eaeaea"}><AiOutlinePicture size={70} /></TypeSetButton>
+//         <TypeSetButton setType={setTypeToLinkOnly} name={"Link only"} height={75} bg={"white"} shadow={"xl"} border={"1px solid #eaeaea"} minW={250} Mx={10000}/>
+//         {/* 余白調整の空タグ */}
+//         <Box w="100%"></Box>
+//         <Box w="100%"></Box>
+//         <Box w="100%"></Box>
+//       </Flex>
+//     </>
+//   )
+// }
 
-const TypeSetButton = ({setType, name, height, children, bg, shadow, border, minW = 250, Mx = 10}: TypeSetButtonProptype) => {
-  return (
-    <Center 
-    onClick={ setType }
-    display="flex"
-    flexDir="column"
-    bg={bg}
-    h={height} w={250} mx={Mx} my={2} minW={minW}
-    boxShadow={shadow} borderRadius={30}
-    border={border}
-    _hover={{ 
-      bg: "yellow.100",
-      filter: "grayscale(0.3)",
-      transition: ".7s",
-    }}
-    >
-      { children ? <Center color="orange.300" mb={10}>{children}</Center> : null } 
-      <Text fontSize="1.3rem">{name}</Text>
-    </Center>
-  )
-}
+// const TypeSetButton = ({setType, name, height, children, bg, shadow, border, minW = 250, Mx = 10}: TypeSetButtonProptype) => {
+//   return (
+//     <Center 
+//     onClick={ setType }
+//     display="flex"
+//     flexDir="column"
+//     bg={bg}
+//     h={height} w={250} mx={Mx} my={2} minW={minW}
+//     boxShadow={shadow} borderRadius={30}
+//     border={border}
+//     _hover={{ 
+//       bg: "yellow.100",
+//       filter: "grayscale(0.3)",
+//       transition: ".7s",
+//     }}
+//     >
+//       { children ? <Center color="orange.300" mb={10}>{children}</Center> : null } 
+//       <Text fontSize="1.3rem">{name}</Text>
+//     </Center>
+//   )
+// }
