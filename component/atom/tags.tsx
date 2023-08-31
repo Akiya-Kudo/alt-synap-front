d66f6a33d2524df1605bbc499e99451c044fbf65
USER <user@USERnoMacBook-Air.local>
2023-08-31 21:56:51 +0900

    truncated

--- component/atom/tags.tsx
+++ component/atom/tags.tsx
@@ -81,7 +81,7 @@ export const NeumTag = ({
             // <Box bg={colorScheme} borderRadius={"full"} mr={1} ml={1} opacity={0.5} h={2} w={2}/> 
             <Box mr={.5} ps={1}><FaHashtag color={colorScheme} opacity={0.7} fontSize={"12px"}/></Box>
             }
-            <TagLabel fontSize={".6rem"} fontWeight={"bold"} color={"text_light"} mr={1}>{display_name}</TagLabel>
+            <TagLabel fontSize={".6rem"} fontWeight={"bold"} color={"text_light"} mr={1}>{display_name?.slice(0, 15) + "..."}</TagLabel>
         </Tag>
     )
 }
